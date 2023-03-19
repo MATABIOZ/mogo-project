16c8820b212d8afd11d2940744f58a8dd8450512
MATABIOZ <6387125@gmail.com>
2023-03-19 19:00:20 +0300

    Added interactive accordion

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+// burger and sidebar
+
 const headerBurger = document.querySelector('.header__burger')
 const sidebar = document.querySelector('.sidebar')
 const allHeaderBurger = document.querySelectorAll('.header__burger')
@@ -12,3 +14,19 @@ allLinks.forEach(item => item.addEventListener('click', (event) => {
 		headerBurger.classList.remove('header__burger_checked');
 		sidebar.classList.remove('sidebar_checked');
 }))
+
+// accordion
+
+const serviceTabs = document.querySelector('.service__tabs');
+const tabs = document.querySelectorAll('.tab');
+
+serviceTabs.addEventListener('click', event => {
+	const target = event.target;
+	if (target.classList.contains('tab__header') || target.closest('.tab__header')) {
+		tabs.forEach(tab => tab.classList.remove('tab_checked'));
+		const parentTab = target.closest('.tab');
+		if (parentTab) {
+			parentTab.classList.add('tab_checked');
+		}
+	}
+})
