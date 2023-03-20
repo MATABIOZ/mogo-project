0744bc419a2b1c30771c479e6333479ad0d5c0b3
MATABIOZ <6387125@gmail.com>
2023-03-20 11:19:00 +0300

    Fixed sliders

--- js/slider.js
+++ js/slider.js
@@ -1,18 +1,43 @@
-const swiper = new Swiper('.slider__inner', {
+const swiper1 = new Swiper('.slider-top', {
 	navigation: {
-		nextEl: '.slider__arrow-next',
-		prevEl: '.slider__arrow-prev',
+		nextEl: '.arrow-next1',
+		prevEl: '.arrow-prev1',
 	},
 	pagination: {
-		el: '.swiper-pagination',
+		el: '.pagination1',
 		clickable: true,
 	},
 	mousewheel: {
 		sensitivity: 1,
-		eventsTarget: '.slider__inner',
+		eventsTarget: '.slider-top',
 	},
 	grabCursor: true,
 	spaceBetween: 30,
 	initialSlide: 1,
 	loop: true,
+	observer: true,
+	observeParents: true,
+	observeSlideChildren: true,
+});
+
+const swiper2 = new Swiper('.slider-bottom', {
+	navigation: {
+		nextEl: '.arrow-next2',
+		prevEl: '.arrow-prev2',
+	},
+	pagination: {
+		el: '.pagination2',
+		clickable: true,
+	},
+	mousewheel: {
+		sensitivity: 1,
+		eventsTarget: '.slider-bottom',
+	},
+	grabCursor: true,
+	spaceBetween: 30,
+	initialSlide: 1,
+	loop: true,
+	observer: true,
+	observeParents: true,
+	observeSlideChildren: true,
 });
