f07df50c4faf04030af60a6dc6470250d57ad50e
MATABIOZ <6387125@gmail.com>
2023-03-20 03:49:21 +0300

    Added build

---
+++ dist/index.7df117ac.js
@@ -0,0 +1,2 @@
+const swiper=new Swiper(".slider__inner",{navigation:{nextEl:".slider__arrow-next",prevEl:".slider__arrow-prev"},pagination:{el:".swiper-pagination",clickable:!0},mousewheel:{sensitivity:1,eventsTarget:".slider__inner"},grabCursor:!0,spaceBetween:30,initialSlide:1,loop:!0});
+//# sourceMappingURL=index.7df117ac.js.map
